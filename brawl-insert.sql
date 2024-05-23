beab16f485902e7dcc896a826f556350cbb5de9f
boniiilla <c.bonillac00@gmail.com>
2024-05-23 10:03:58 +0200

    añadir

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -6,6 +6,6 @@ IGNORE 1 LINES
 (nom_rareza, color);
 
 LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/tipus.csv' INTO TABLE tipus
-FIELDS TERMINATED BY '\t' ENCLOSED BY '"'
+FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (nom_tipus, descripcio);
