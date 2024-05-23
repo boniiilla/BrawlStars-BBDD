0328781397398f89cd6c3290929e45dd5229ac9c
boniiilla <c.bonillac00@gmail.com>
2024-05-23 11:57:59 +0200

    temorary table + insert

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -10,3 +10,27 @@ FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (nom_tipus, descripcio);
 
+LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/tipus.csv' INTO TABLE tipus
+FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(nom_tipus, descripcio);
+
+DROP TEMPORARY IF EXISTS temp_pers;
+CREATE TEMPORARY  TABLE IF NOT EXISTS temp_pers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(24),
+    id_rareza INT,
+    nom_rareza VARCHAR(24),
+    id_tipus INT,
+    nom_tipus VARCHAR(24),
+    foto LONGBLOB,
+    audio VARCHAR(64),
+    descripcio TEXT,
+    FOREIGN KEY (id_rareza) REFERENCES rarezas(id_rareza),
+    FOREIGN KEY (id_tipus) REFERENCES tipus(id_tipus)
+);
+
+LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/personatges.csv' INTO TABLE temp_pers
+FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(nom, @dummy, @dummy, nom_tipus, nom_rareza, descripcio);
