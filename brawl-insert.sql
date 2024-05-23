1548ec74a3a03a944d5897c62f17afddbdc73ee9
boniiilla <c.bonillac00@gmail.com>
2024-05-23 23:39:25 +0200

    arreglado

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -292,8 +292,8 @@ WHERE id_forza >= 8;
 UPDATE forza_brawler fb
 SET id_ref2 = 
     CASE 
-        WHEN id_pers = 1 THEN (SELECT id_ref FROM (SELECT id_ref FROM reforcos WHERE id_ref NOT IN (
-            SELECT id_ref1 FROM forza_brawler WHERE id_pers = 1) ORDER BY RAND() LIMIT 1) AS refuerza)
+        WHEN id_pers = 1 THEN (SELECT id_ref FROM reforcos WHERE id_ref NOT IN (
+            SELECT id_ref1 FROM forza_brawler WHERE id_pers = 1 and id_forza >= 10) ORDER BY RAND() LIMIT 1) AS refuerzos
         WHEN id_pers = 2 THEN (SELECT id_ref FROM (SELECT id_ref FROM reforcos WHERE id_ref NOT IN (
             SELECT id_ref1 FROM forza_brawler WHERE id_pers = 2) ORDER BY RAND() LIMIT 1) AS refuerzos)
         WHEN id_pers = 3 THEN (SELECT id_ref FROM (SELECT id_ref FROM reforcos WHERE id_ref NOT IN (
