0ecf454a11598c00f601901e7d4195251c7d9164
boniiilla <c.bonillac00@gmail.com>
2024-05-23 10:12:09 +0200

    quitar @

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -3,4 +3,4 @@ USE brawl;
 LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/rarezas.csv' INTO TABLE rarezas
 FIELDS TERMINATED BY ';' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(@nom_rareza, @color);
+(nom_rareza, color);
