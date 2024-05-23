7779bbe5198b986ff2aadd176112667d8091c2f0
boniiilla <c.bonillac00@gmail.com>
2024-05-23 10:07:35 +0200

    cambios

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -4,8 +4,3 @@ LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/rarezas.csv' INT
 FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (nom_rareza, color);
-
-LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/tipus.csv' INTO TABLE tipus
-FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(nom_tipus, descripcio);
