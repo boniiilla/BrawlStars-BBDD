aa50acf37e09694b60267924d465cb7d008c9746
boniiilla <c.bonillac00@gmail.com>
2024-05-23 09:20:59 +0200

    primer insert

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -1,2 +1,6 @@
 USE brawl;
 
+LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/personatges.csv' INTO TABLE rarezas
+FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@dummy, @dummy, @dummy, @dummy, nom_rareza);
