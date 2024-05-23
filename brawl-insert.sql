b153fc5cffe33c2e145a307f0e8993ca4353afdc
boniiilla <c.bonillac00@gmail.com>
2024-05-23 09:26:16 +0200

    Comun

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -4,3 +4,7 @@ LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/personatges.csv'
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (@dummy, @dummy, @dummy, @dummy, nom_rareza);
+
+UPDATE rarezas
+SET color = "Gris"
+WHERE nom_rareza = "Comun";
