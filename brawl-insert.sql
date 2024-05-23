5adf7f09d27bd552fec79a313ce80c2b672e934b
boniiilla <c.bonillac00@gmail.com>
2024-05-23 10:43:13 +0200

    prueba

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -3,10 +3,10 @@ USE brawl;
 LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/rarezas.csv' INTO TABLE rarezas
 FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(@dummy, color);
+(nom_rareza);
 
 
 LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/tipus.csv' INTO TABLE tipus
-FIELDS TERMINATED BY '\t' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n'
+FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(nom_tipus, descripcio);
+(nom_tipus);
