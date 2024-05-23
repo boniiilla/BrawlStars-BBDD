c9639c7b66c0d174cb14c8bf43b48bf38933b6b0
boniiilla <c.bonillac00@gmail.com>
2024-05-23 10:44:44 +0200

    color

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -3,7 +3,7 @@ USE brawl;
 LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/rarezas.csv' INTO TABLE rarezas
 FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(nom_rareza);
+(nom_rareza, color);
 
 
 LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/tipus.csv' INTO TABLE tipus
