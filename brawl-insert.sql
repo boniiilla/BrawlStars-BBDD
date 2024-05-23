cb7d39ee888f23f2d1b9f3a44b459489cc450769
boniiilla <c.bonillac00@gmail.com>
2024-05-23 10:46:48 +0200

    lines terminated

--- brawl-insert.sql
+++ brawl-insert.sql
@@ -1,7 +1,7 @@
 USE brawl;
 
 LOAD DATA LOCAL INFILE '/home/usuari/compartida/BrawlStars-BBDD/rarezas.csv' INTO TABLE rarezas
-FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\t'
+FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (nom_rareza, color);
 
